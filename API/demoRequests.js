316fd1c1ddb55422167865a6e8f0170cc0b1df9f
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-18 19:18:24 +0300

    Add dummy data for demo testing

---
+++ API/demoRequests.js
@@ -0,0 +1,19 @@
+const Samson = {
+    agent_id: '246TI3',
+    password: 'Samson!234',
+}
+
+const Cynthia = {
+    agent_id: '247T13',
+    password: 'Cynthia!234',
+}
+
+const Ian = {
+    agent_id: '248T23',
+    password: 'Ian!2345',
+}
+
+const Judy = {
+    agent_id: '249T33',
+    password: 'Judy!234',
+}
