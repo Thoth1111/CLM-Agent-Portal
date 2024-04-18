35811459c5369bdfecc81d3bd8c56f02289466de
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-18 19:47:55 +0300

    Update Input component

--- components/TextIconInput.js
+++ components/TextIconInput.js
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 import * as Icon from 'react-native-feather';
 import { AuthInputLabel, AuthTextInput, LeftIcon, RightIcon, Colors } from './styles';
 
-const { green, jet } = Colors;
+const { green, jet, platinum } = Colors;
 
 const TextIconInput = ({ label, iconName, isPassword, hidePassword, setHidePassword, ...props }) => {
     const DynamicIcon = Icon[iconName];
@@ -12,13 +12,13 @@ const TextIconInput = ({ label, iconName, isPassword, hidePassword, setHidePassw
     return (
         <View>
             <LeftIcon>
-                <DynamicIcon size={25} color={green} />
+                <DynamicIcon size={25} color={platinum} />
             </LeftIcon>
             <AuthInputLabel>{label}</AuthInputLabel>
             <AuthTextInput {...props} />
             {isPassword && (
                 <RightIcon onPress={() => setHidePassword(!hidePassword)}>
-                    {hidePassword ? <EyeOff size={30} color={green} /> : <EyeOn size={30} color={green} />}
+                    {hidePassword ? <EyeOff size={30} color={platinum} /> : <EyeOn size={30} color={platinum} />}
                 </RightIcon>
             )}
         </View>
