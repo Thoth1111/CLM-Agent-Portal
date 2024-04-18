8fdbb28d703a0eac9522ebc73f47b568c45dffb8
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-18 19:15:33 +0300

    Create validation helpers for authentication

---
+++ helpers/validations.js
@@ -0,0 +1,17 @@
+export const validAgentId = (agentId) => {
+    if (agentId.length < 5) {        
+        console.log('Agent ID must be at least 5 characters long');
+        return false;
+    }
+    return true;
+}
+
+export const validPassword = (password) => {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,}$/;
+    if (password.length === 0) {
+        return false;
+    } else if (!passwordRegex.test(password)) {
+        return false;
+    }
+    return true;
+}
