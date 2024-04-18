f96476093340749552e8b5e9d0d7df1a92b0a22e
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-18 19:14:50 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import RootStack from './navigators/RootStack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AgentContext } from './components/AgentContext';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Splash from './components/Splash';
 
 export default function App() {
+  const [appReady, setAppReady] = useState(false);
+  const [agentData, setAgentData] = useState(null);
+
+  const checkAgentData = async () => {
+    await AsyncStorage.getItem('agentSessionData')
+      .then((agentSessionData) => {
+        if (agentSessionData !== null) {
+          setAgentData(JSON.parse(agentSessionData));
+        } else {
+          setAgentData(null);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setAppReady(true);
+      })
+  }
+
+  useEffect(() => {
+    checkAgentData();
+  }, []);
+
+  if (!appReady) {
+    return <Splash />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <AgentContext.Provider value={{ agentData, setAgentData }}>
+        <RootStack />
+      </AgentContext.Provider>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
